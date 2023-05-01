56ba33d2f15bf37066ceab092fc6d564a32317fd
kirill-gbn <papayok@mail.ru>
2023-05-01 18:27:15 +0400

    fix: change connected pages

--- building-site/src/App.js
+++ building-site/src/App.js
@@ -2,11 +2,12 @@ import logo from './logo.svg';
 import './App.css';
 import React, { Component }  from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Header from './Components/Header';
 
 function App() {
   return (
     <div>
-      
+      <Header />
     </div>
   );
 }
--- building-site/src/Components/Header.js
+++ building-site/src/Components/Header.js
@@ -3,41 +3,57 @@ import { Button, Container, Form, FormControl, Nav, NavLink, Navbar, NavbarBrand
 import logo from './Dismantling.png'
 import NavbarToggle from "react-bootstrap/esm/NavbarToggle";
 import NavbarCollapse from "react-bootstrap/esm/NavbarCollapse";
+import { Routes, BrowserRouter as Router, Route, Link } from "react-router-dom";
+import Home from "../Pages/Home";
+import About from "../Pages/About";
+import Feedback from "../Pages/Feedback";
+import Contacts from "../Pages/Contacts";
 
 export default class Header extends Component {
     render() {
-        
+
         return (
-            <Navbar collapseOnSelect expand="md" bg="dark" variant="dark">
-                <Container>
-                    <NavbarBrand href="/">
-                        <img 
-                            src={logo}
-                            height="70"
-                            width="70"
-                            className="d-inline-block align-top"
-                            alt="Logo"
-                        />
-                    </NavbarBrand>
-                    <NavbarToggle aria-controls="responsive-navbar-nav" />
-                    <NavbarCollapse id="responsive-navbar-nav" >
-                        <Nav className="mr-auto">
-                            <NavLink href="/" > Главная страница </NavLink>
-                            <NavLink href="/about" > О нас </NavLink>
-                            <NavLink href="/feedback" > Отзывы </NavLink>
-                            <NavLink href="/contacts" > Контакты </NavLink>
-                        </Nav>
-                    </NavbarCollapse>
-                    <Form inline >
-                            <FormControl
-                                type="text"
-                                placeholder="Поиск"
-                                className="mr-sm-2"
-                            />
-                        </Form>
-                        <Button variant="outline-info">Найти</Button>
-                </Container>
-            </Navbar>
-          );
+            <>
+                <Navbar sticky="top" collapseOnSelect expand="md" bg="dark" variant="dark">
+                    <Container>
+                        <NavbarBrand href="/">
+                            <img
+                                src={logo}
+                                height="50"
+                                width="50"
+                                className="d-inline-block align-top"
+                                alt="Logo"
+                            />Демонтаж зданий
+                        </NavbarBrand>
+                        <NavbarToggle aria-controls="responsive-navbar-nav" />
+                        <NavbarCollapse id="responsive-navbar-nav" >
+                            <Nav className="me-auto">
+                                <NavLink href="/" > Главная страница </NavLink>
+                                <NavLink href="/about" > О нас </NavLink>
+                                <NavLink href="/feedback" > Отзывы </NavLink>
+                                <NavLink href="/contacts" > Контакты </NavLink>
+                            </Nav>
+                            <Form className="inline">
+                                <FormControl
+                                    type="text"
+                                    placeholder="Поиск"
+                                    className="mr-sm-2"
+                                />
+                            </Form>
+                            <Button variant="outline-info">Найти</Button>
+                        </NavbarCollapse>
+                    </Container>
+                </Navbar>
+
+                <Router>
+                    <Routes>
+                        <Route path="/" element={<Home/>} />
+                        <Route path="/about" element={<About/>} />
+                        <Route path="/feedback" element={<Feedback/>} />
+                        <Route path="/contacts" element={<Contacts/>} />
+                    </Routes>
+                </Router>
+            </>
+        );
     }
 }
