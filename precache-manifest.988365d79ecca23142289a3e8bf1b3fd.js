89cd7a909d0e097601970320c3305510b5b3e209
kirill-gbn <papayok@mail.ru>
2023-05-03 21:37:04 +0400

    Updates

---
+++ precache-manifest.988365d79ecca23142289a3e8bf1b3fd.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "86972b2dcd10c5247275",
+    "url": "/react-website/static/css/main.f0f435e1.chunk.css"
+  },
+  {
+    "revision": "86972b2dcd10c5247275",
+    "url": "/react-website/static/js/main.25d9b3fd.chunk.js"
+  },
+  {
+    "revision": "26c6adf437089070e4ca",
+    "url": "/react-website/static/js/runtime~main.b1e85587.js"
+  },
+  {
+    "revision": "c0d107d87860b2e1ad5a",
+    "url": "/react-website/static/css/2.e5250dcb.chunk.css"
+  },
+  {
+    "revision": "c0d107d87860b2e1ad5a",
+    "url": "/react-website/static/js/2.a615fa3f.chunk.js"
+  },
+  {
+    "revision": "049481630dfb12e5a9f3",
+    "url": "/react-website/static/js/3.b25c81c5.chunk.js"
+  },
+  {
+    "revision": "987f842c3cca9902b932177b6bcd59ba",
+    "url": "/react-website/static/media/Dismantling.987f842c.png"
+  },
+  {
+    "revision": "b586a94f91d0330a1be62de8630e6ee2",
+    "url": "/react-website/static/media/demolition1.b586a94f.jpg"
+  },
+  {
+    "revision": "3d917adaa76547a9cd0d7a89bc414eeb",
+    "url": "/react-website/static/media/demolition2.3d917ada.jpg"
+  },
+  {
+    "revision": "7fb311d5e83a49d177dc5f2ea58707af",
+    "url": "/react-website/static/media/demolition3.7fb311d5.jpg"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/react-website/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "9f2c28ba9dc69855157a3f2abbb6a910",
+    "url": "/react-website/index.html"
+  }
+];
